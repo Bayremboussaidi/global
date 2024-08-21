9d17ed708ee5a84aecaf49293ccfba6a46999f83
bayrem <bayremboussaidi187@gmail.com>
2024-08-21 13:50:06 +0100

    njareb

--- back/server.js
+++ back/server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/', apiRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8084;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
