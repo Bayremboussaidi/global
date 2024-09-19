678a729bbc79647c3cc7bc01fe66b756678f078e
bayrem <bayremboussaidi187@gmail.com>
2024-09-19 23:42:45 +0100

    11

--- front/src/app/ADMIN/show-rec/show-rec.component.ts
+++ front/src/app/ADMIN/show-rec/show-rec.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { ReclamationService } from '../../services/reclamation.service';
 import { Router } from '@angular/router';
 
@@ -22,7 +22,51 @@ export class ShowReclamationComponent implements OnInit {
   }
 
   toggleReclamations() {
-    this.displayReclamations  = true ;/*= !this.displayReclamations;*/
+    this.displayReclamations  = true ;
+
+    if (this.displayReclamations) {
+      this.loadReclamations();
+    }
+  }
+
+  loadReclamations() {
+    this.reclamationService.getAllReclamations().subscribe(
+      (data: any[]) => {
+        this.reclamationList = data;
+      },
+      (error) => {
+        console.error('Error fetching reclamations', error);
+      }
+    );
+  }
+}*/
+
+import { Component, OnInit } from '@angular/core';
+import { ReclamationService } from '../../services/reclamation.service';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-show-reclamation',
+  templateUrl: './show-rec.component.html',
+  styleUrls: ['./show-rec.component.css']
+})
+export class ShowReclamationComponent implements OnInit {
+  reclamationList: any[] = []; // Replace with your data model if available
+  displayReclamations: boolean = false;
+displayedColumns: any;
+
+  constructor(private reclamationService: ReclamationService, private router: Router) { }
+
+  ngOnInit(): void {
+    this.toggleReclamations();
+  }
+
+  goToCentralPage() {
+    this.router.navigate(['/dash-admin']);
+  }
+
+  toggleReclamations() {
+    this.displayReclamations = !this.displayReclamations;
 
     if (this.displayReclamations) {
       this.loadReclamations();
